9da1576b1a6c8945ab0d797874c59be2f4ccffbe
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-24 06:07:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
     element = "wow"
     array.unshift(element)
-    puts 
+    puts array.inspect
   
 end
 
